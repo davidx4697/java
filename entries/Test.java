884856b36869da6f8fe3ab906c8f6cf14d4d07b4
davidx4697 <davidx4697@hstat.org>
2018-05-21 03:11:28 +0000

    done 8

--- entries/Test.java
+++ entries/Test.java
@@ -1,4 +1,4 @@
-package hstatboard;
+package hstatBoard;
 
 /* package hstatboard;
 import java.util.Scanner;
@@ -16,8 +16,10 @@ public class other {
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.*;
 import java.applet.Applet;
+import java.awt.event.KeyEvent;
 
 
 import javax.swing.JButton;
@@ -26,7 +28,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JFrame;
-import java.util.ArrayList;
 
 public class other extends JFrame{
 	JPanel thoughtpanel = new JPanel();
@@ -36,14 +37,12 @@ public class other extends JFrame{
 	JTextField thoughttext = new JTextField(30); //30 means length of field horizontally 
 	JButton thoughtbutton = new JButton("Enter");
 	
-	
 	public other() {
-		
 		setTitle("hstatBoard");
 		setVisible(true);
 		setSize(600,600);
+		thoughttext.requestFocusInWindow();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
 		
 		thoughtpanel.add(mindlabel); //What's on your mind on the left
 		thoughtpanel.add(thoughttext);
@@ -53,36 +52,90 @@ public class other extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				String input = thoughttext.getText();
 				thoughtlabel.setText(input);
-				thoughttext.requestFocusInWindow();
 			}
 		});
 		
 		thoughtpanel.add(thoughtbutton);
 		
-		thoughtbutton.addActionListener(new ActionListener() { //button listener
+		thoughtbutton.addActionListener(new ActionListener() {  //button click
 			public void actionPerformed(ActionEvent e) {
-				String input = thoughttext.getText();
-				thoughtlabel.setHorizontalAlignment(JLabel.CENTER);
-				//replylabel.setHorizontalAlignment(JLabel.LEFT);
-				thoughtlabel.setText(input); //fix input to center!	
-				thoughttext.setText("");
-				thoughttext.requestFocusInWindow();
+				int thoughtarray = 1; //thoughtarray 
+				for (int i=0; i <100; i++) {
+					//thoughtlabel.setText(input);
+					JLabel label = new JLabel(String.valueOf(thoughtarray));
+					label.setText(String.valueOf(thoughtlabel));
+					thoughtarray++;
+					thoughtlabel.add(label);
+					String input = thoughttext.getText();
+					thoughtlabel.setHorizontalAlignment(JLabel.CENTER);
+					replylabel.setHorizontalAlignment(JLabel.LEFT);
+					//thoughtlabel.setText(input); //fix input to center!
+					thoughttext.setText("");
+					thoughttext.requestFocusInWindow();
+				}
+
 				
-				ArrayList<String> thoughtsArray = new ArrayList <String>();
-				thoughtsArray.add(input);
-				System.out.println(thoughtsArray);
 				
 			}
 		});
-		
-		
+	
 		thoughtpanel.add(thoughtlabel);
 		add( thoughtpanel);
-		
-		
+	
 	}
 	public static void main (String [] args) {
 		other board = new other();
 	}
 }  
-		
+
+
+
+
+
+
+
+/*
+package hstatBoard;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+
+public class BoxLabels {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run() {
+                Box box = Box.createVerticalBox();
+                Font font = new Font("monospaced", Font.PLAIN, 11);
+
+                JPanel sideBar = new JPanel();
+                sideBar.setBackground(Color.BLACK);
+                sideBar.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+                sideBar.add(box);
+
+                JTextArea text = new JTextArea(15, 50);
+                text.setMargin(new Insets(5, 5, 5, 5));
+                text.setBackground(Color.darkGray);
+                text.setForeground(Color.white);
+                text.setFont(font);
+                String hello = "hi";
+                int count = 1;
+                for (int i = 0; i < 100; i++) {
+                	//String hello = "hi";
+                    JLabel label = new JLabel(String.valueOf(hello));
+                    label.setFont(font);
+                    label.setForeground(Color.GREEN);
+                    box.add(label);
+                    count++;
+                }
+
+                JPanel panel = new JPanel(new BorderLayout());
+                panel.add(text);
+                panel.add(sideBar, BorderLayout.WEST);
+
+                JOptionPane.showMessageDialog(null, panel);
+            }
+        });
+    }
+}
+*/
---
+++ entries/entry8.md
@@ -0,0 +1,31 @@
+# Entry 8
+
+### Message clients
+My MVP is a primitive version of a messenger client made in pure java. There is a problem with it being made in pure java and that it's impossible to make this into a website. I've tried using Tomcat, a free to use server that runs java but it cannot run pure java. It can only accept applications written in jsp, a java language with html integrated. JSP solely used for web development and is kind of similar to Java. I will attempt to rewrite my project in JSP so i can run it on a local host webpage rather on a java app.  
+
+### Are templates cool?
+Although templates are amazing and save time, I really wanted to code my MVP from scratch. I wanted to know just how much that i've learned could be implemented into my MVP. Now that theres a week left from presentation, I've started to regret that decision. Since I started from scratch my code is all over the place and my variable names are inefficient. Im currently changing variable names, fixing indentation, and commenting my code to make it presentable friendly.
+
+
+### Challenges 
+Some of the challenges I faced today was creating a for loop that takes the user's input and makes it equal to a JLABEL. A JLABEL is like a variable for those unfamiliar with Java GUI. 
+
+``` java 
+for (int i = 0; i < thoughts.size(); i++) {			
+                     			System.out.println(thoughts.get(i));
+                     			String thoughtsword = thoughts.get(i);
+                     			
+                     			JLabel thoughtwordlabel =new JLabel(thoughtsword);
+                     			thoughtwordlabel.setText("<html> <br>" +thoughtsword + " <br> </html>");
+                     			thoughtpanel.add(thoughtwordlabel);
+                     			thoughts.remove(0);
+```
+
+This for loop successfully grabs and creates a new JLabel using the user's input except there's a small problem. JLabels do not accept line breaks or go to the next line by default. It seems after googling the only way to make a user's input print on a seperate line is by creating a JTextArea. The syntax for a JTextArea is different and I will learn it in the next couple of days. 
+
+Challenge:
+<img src="../images/challenge ent8.png"/>
+
+### Takeaways
+
+The only takeaway I had this week is to start making your code presentable. I decided to fix the indentation in my project and added more comments so that people can see the way I think or the reasons why I wrote that line of code. This will make it easier to understand when I present infront of my classmates. 
